46e2fbd0f73ddf996e28e6d287a624aa05548227
Josacc <thomas3571113@gmail.com>
2021-09-23 18:18:47 -0500

    para desplegar un grafico tipo plotly se debe ocupar la función en el UI: "plotlyOutput()"

--- box-plot/ui.R
+++ box-plot/ui.R
@@ -17,9 +17,9 @@ shinyUI(fluidPage(
 
             numericInput("n_hoja" ,
                          "Número de hoja" ,
-                         value = 2 ,
-                         min = 2 ,
-                         max = 7) ,
+                         value = 1 ,
+                         min = 1 ,
+                         max = 2) ,
 
             numericInput("n_col" ,
                          "Número de columna" ,
@@ -29,7 +29,40 @@ shinyUI(fluidPage(
         ),
 
         mainPanel(
-            plotOutput("distPlot")
+            plotlyOutput("plot")
         )
     )
 ))
+
+
+
+
+
+
+
+
+
+
+
+#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
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
